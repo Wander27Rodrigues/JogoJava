97de4c77fa9cd447f783e5b3fe0eea32b1e48252
Wander Souza <wanderrodrigues_@outlook.com>
2021-05-17 22:36:24 -0300

    :brain: initial

---
+++ jogo.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.Random;
+
+    public  class jogo {
+            public  static void  main(String [] args){
+                PrintSteam show = System.out;
+                Scanner scan = new Scanner(System.in);
+                Random gerador = new Random();
+            }
+}
